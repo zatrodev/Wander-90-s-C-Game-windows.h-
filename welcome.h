359508d9dcecb8a30189dce8b4c19fffc87a3be4
zatro <79815768+zatrodev@users.noreply.github.com>
2021-05-16 14:38:19 +0800

    Update welcome.h

--- welcome.h
+++ welcome.h
@@ -12,34 +12,33 @@ bool welcome()
     hideCursor();
 
     int columns, rows;
-    setWindowSize(500, 200);
     getConsoleSize(columns, rows);
 
-    gotoxy(columns / 2, (rows / 2) - 9);
+    gotoxy((columns / 2) - 35, (rows / 2) - 9);
     cout << " __      __                    .___";
 
-    gotoxy(columns / 2, (rows / 2) - 8);
+    gotoxy((columns / 2) - 35, (rows / 2) - 8);
     cout << "/  \\    /  \\_____    ____    __| _/___________";
 
-    gotoxy(columns / 2, (rows / 2) - 7);
+    gotoxy((columns / 2) - 35, (rows / 2) - 7);
     cout << "\\   \\/\\/   /\\__  \\  /    \\  / __ |/ __ \\_  __ \\";
 
-    gotoxy(columns / 2, (rows / 2) - 6);
+    gotoxy((columns / 2) - 35, (rows / 2) - 6);
     cout << " \\        /  / __ \\|   |  \\/ /_/ \\  ___/|  | \\/";
 
-    gotoxy(columns / 2, (rows / 2) - 5);
+    gotoxy((columns / 2) - 35, (rows / 2) - 5);
     cout << "  \\__/\\  /  (____  /___|  /\\____ |\\___  >__|";
 
-    gotoxy(columns / 2, (rows / 2) - 4);
+    gotoxy((columns / 2) - 35, (rows / 2) - 4);
     cout << "       \\/        \\/     \\/      \\/    \\/  ";
 
-    gotoxy((columns / 2) + 16, (rows / 2) - 1);
+    gotoxy((columns / 2) - 18, (rows / 2) - 1);
     cout << "E A S Y  M O D E";
-    gotoxy((columns / 2) + 16, (rows / 2) + 1);
+    gotoxy((columns / 2) - 18, (rows / 2) + 1);
     cout << "N O R M A L  M O D E";
-    gotoxy((columns / 2) + 16, (rows / 2) + 3);
+    gotoxy((columns / 2) - 18, (rows / 2) + 3);
     cout << "H A R D  M O D E";
-    gotoxy((columns / 2) + 16, (rows / 2) + 5);
+    gotoxy((columns / 2) - 18, (rows / 2) + 5);
     cout << "W A N D E R  M O D E";
 
     if (kbhit())
@@ -47,7 +46,7 @@ bool welcome()
         switch (getch())
         {
         case 'w':
-            gotoxy((columns / 2) + 12, (rows / 2) + i);
+            gotoxy((columns / 2) - 22, (rows / 2) + i);
             cout << "  ";
 
             if (i != -1)
@@ -55,7 +54,7 @@ bool welcome()
 
             break;
         case 's':
-            gotoxy((columns / 2) + 12, (rows / 2) + i);
+            gotoxy((columns / 2) - 22, (rows / 2) + i);
             cout << "  ";
 
             if (i != 5)
@@ -76,10 +75,10 @@ bool welcome()
         }
     }
 
-    gotoxy((columns / 2) + 12, (rows / 2) + i);
+    gotoxy((columns / 2) - 22, (rows / 2) + i);
     cout << ">>";
 
-    gotoxy((columns / 2) + 14, (rows / 2) + 9);
+    gotoxy((columns / 2) - 20, (rows / 2) + 9);
     cout << "Press [ENTER] to Start";
 
     return true;
