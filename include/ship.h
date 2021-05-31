ed688d451be86bf7cb2401ba18f2a523cdaab2ed
zatro <79815768+zatrodev@users.noreply.github.com>
2021-05-31 13:20:25 +0800

    Update ship.h

--- include/ship.h
+++ include/ship.h
@@ -1,19 +1,24 @@
 #pragma once
+#include <cmath>
 #include <iostream>
 #include <conio.h>
 #include <vector>
+#include <time.h>
 #include "utils.h"
 #include "welcome.h"
 using namespace std;
 
-struct Ship 
+struct Ship
 {
     vector<vector<float>> ship_coords;
-    float shipX, shipY, shipSpeed = 2;
+    float shipX, shipY, shipSpeed;
+    bool isEnemy;
 
-    Ship(float shipX, float shipY) 
+    Ship(float shipX, float shipY, bool isEnemy = false)
         : shipX(shipX)
         , shipY(shipY)
+        , isEnemy(isEnemy)
+        , shipSpeed(isEnemy ? 0.2 : 2)
     {
     }
 
@@ -25,10 +30,20 @@ struct Ship
 
     void draw()
     {
-        gotoxy(shipX, shipY);
-        cout << "_____";
-        gotoxy(shipX, shipY + 1);
-        cout << ":</__|";
+        if (isEnemy)
+        {
+            gotoxy(shipX, shipY);
+            cout << "_____";
+            gotoxy(shipX, shipY + 1);
+            cout << "|__\\<:";
+        }
+        else
+        {
+            gotoxy(shipX, shipY);
+            cout << "_____";
+            gotoxy(shipX, shipY + 1);
+            cout << ":</__|";
+        }
     }
 
     void set_ship_coords()
@@ -54,5 +69,34 @@ struct Ship
         cout << "     ";
         gotoxy(shipX, shipY + 1);
         cout << "      ";
+    }
+
+    void move(int rows)
+    {
+        static bool isUp = true;
+        static int count = 1;
+        static int changeDir = rand() % 50 + 40;
+
+        if (round(shipY) == 3 || round(shipY + 1) == rows - 2 || count % changeDir == 0){
+            isUp = isUp ? false : true;
+
+            changeDir = rand() % 50 + 40;
+        }
+        
+        if (!isUp)
+        {
+            _delete();
+            shipY -= shipSpeed;
+            draw();
+        }
+
+        else if (isUp)
+        {
+            _delete();
+            shipY += shipSpeed;
+            draw();
+        }
+        
+        ++count;
     }
 };
